fe062e7a68464c2f0e705710417b9b0fcb13fe90
@charithakaush <charithakaush@gmail.com>
2024-08-24 14:15:18 +0200

    Express app with Sequelize and SQLite

---
+++ migrations/20240824113231-create-items-table.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Items', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      quantity: {
+        type: Sequelize.INTEGER, 
+        allowNull: false,
+        defaultValue: 0
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: true 
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Items');
+  }
+};

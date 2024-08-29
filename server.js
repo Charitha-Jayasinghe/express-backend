148b9a6c196f3260e0d061e5b0d788796f3e60eb
@charithakaush <charithakaush@gmail.com>
2024-08-29 09:12:13 +0200

    asdasdsa

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { Sequelize, DataTypes } = require('sequelize');
 const path = require('path'); // Import path module for resolving paths
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(cors());
+
 
 
 const sequelize = new Sequelize({
